11da082d7587dae9b74aa96851704d3a12215a53
0-BSCode <bryanipsanchez@gmail.com>
2024-06-27 14:13:33 +0800

    Refactored header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@ import RecipeList from "@/components/organisms/RecipeList";
 
 export default function Home() {
   return (
-    <main className='flex flex-col gap-3'>
-      <section className='flex justify-between'>
-        <div>
-          <h1>Greetings!</h1>
-          <p>What do you want to cook today?</p>
+    <main className='flex flex-col gap-6'>
+      <section className='flex justify-between items-center'>
+        <div className='flex flex-col gap-0 items-center w-full'>
+          <h1 className='text-2xl font-bold text-black'>Greetings!</h1>
+          <p className='text-md italic text-neutral-gray-2'>What do you want to cook today?</p>
         </div>
-        <img src={"https://randomuser.me/api/portraits/thumb/women/39.jpg"} alt='Profile Picture' />
       </section>
-      <h3 className='text-xl font-medium'>Recipes</h3>
+      <div className='flex flex-col gap-1'>
+        <h3 className='text-xl font-medium text-center'>Recipes</h3>
+        <div className='w-full h-1 bg-black' />
+      </div>
       <RecipeList />
     </main>
   );
