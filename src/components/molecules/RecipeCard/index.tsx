86efae38b5cefecba5054b618b246cf6124b8d42
0-BSCode <bryanipsanchez@gmail.com>
2024-06-27 18:08:05 +0800

    Start making card responsive

--- src/components/molecules/RecipeCard/index.tsx
+++ src/components/molecules/RecipeCard/index.tsx
@@ -2,17 +2,16 @@ import Badge from "@/components/atoms/Badge";
 import { Recipe } from "@/types/Recipe.interface";
 import Link from "next/link";
 import { FaClock } from "react-icons/fa";
-import TimeBadge from "../TimeBadge";
 
 const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
   return (
-    <Link href={`/recipes/${recipe.id}`} className='w-fit h-fit'>
-      <div className='relative flex flex-col rounded-lg bg-neutral-gray-4 w-40 h-40 items-center'>
-        <img className='absolute w-full h-full object-cover rounded-lg' src={recipe.imgUrl} alt={recipe.name} />
-        <div className='absolute rounded-lg w-full h-full bg-gradient-to-b from-transparent from-50% to-neutral-black to-95%' />
-        <div className='z-50 w-full h-full flex flex-col justify-between p-2'>
+    <Link href={`/recipes/${recipe.id}`} className='h-fit w-fit'>
+      <div className='relative flex aspect-square w-40 flex-col items-center rounded-lg bg-neutral-gray-4 sm:w-48 md:w-52'>
+        <img className='absolute h-full w-full rounded-lg object-cover' src={recipe.imgUrl} alt={recipe.name} />
+        <div className='absolute h-full w-full rounded-lg bg-gradient-to-b from-transparent from-50% to-neutral-black to-95%' />
+        <div className='z-50 flex h-full w-full flex-col justify-between p-2'>
           <Badge color='white' text={`${recipe.cookTime}m`} icon={<FaClock />} />
-          <p className='text-white font-medium text-md'>{recipe.name}</p>
+          <p className='text-md font-medium text-white'>{recipe.name}</p>
         </div>
       </div>
     </Link>
