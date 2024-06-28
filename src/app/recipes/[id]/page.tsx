1d6166a511b4619be5b024183ee98e316a4f01bf
0-BSCode <bryanipsanchez@gmail.com>
2024-06-28 22:44:03 +0800

    Tweak desktop design

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -10,11 +10,8 @@ export default async function RecipePage({
   };
 }) {
   return (
-    <main className='flex flex-col gap-4 md:gap-8'>
-      <Link
-        href='/'
-        className='flex items-center gap-2 text-md text-neutral-gray-2 md:gap-4 md:text-lg lg:text-xl xl:text-2xl'
-      >
+    <main className='mx-auto flex flex-col gap-4 md:gap-8 lg:w-3/5'>
+      <Link href='/' className='flex items-center gap-2 text-md text-neutral-gray-2 md:gap-4 md:text-lg lg:text-xl'>
         <FaArrowLeft />
         <p>Go Back</p>
       </Link>
