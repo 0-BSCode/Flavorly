70f7e911fa0b050821b9564eb60db353805c9e0c
0-BSCode <bryanipsanchez@gmail.com>
2024-06-28 11:28:11 +0800

    Adjust styling

--- src/components/atoms/List/index.tsx
+++ src/components/atoms/List/index.tsx
@@ -7,7 +7,7 @@ type ListProps = {
 
 const List = ({ title, items, isOrdered }: ListProps) => {
   return (
-    <div className='flex-1'>
+    <div className='flex flex-1 flex-col gap-3'>
       <h4 className='rounded-xl bg-primary-100 py-4 text-center text-2xl font-bold text-white'>{title}</h4>
       <p className='my-2 text-center text-xl text-neutral-gray-1'>{items.length} items</p>
       <ol className='flex list-inside flex-col gap-3'>
--- src/components/organisms/RecipeHeader/index.tsx
+++ src/components/organisms/RecipeHeader/index.tsx
@@ -9,7 +9,7 @@ type RecipeHeader = Omit<Recipe, "id" | "ingredients" | "instructions">;
 
 const RecipeHeader = (recipeInfo: RecipeHeader) => {
   return (
-    <section className='flex flex-col gap-5 md:gap-8 lg:gap-10'>
+    <section className='flex flex-col gap-5 md:gap-8 lg:gap-12'>
       <div className='relative h-52 w-full md:h-60 md:rounded-xl lg:h-72'>
         <img
           className='absolute h-full w-full rounded-[inherit] object-cover'
@@ -18,13 +18,15 @@ const RecipeHeader = (recipeInfo: RecipeHeader) => {
         />
         <div className='absolute h-full w-full rounded-[inherit] bg-gradient-to-b from-transparent from-50% to-neutral-black to-95%' />
       </div>
-      <div className='flex flex-col items-center gap-2 md:gap-1'>
+      <div className='flex flex-col items-center gap-2 md:gap-1 lg:gap-4'>
         <div className='flex w-full xl:justify-center'>
           {/* TODO: Figure out if we should render the remaining badges */}
           <Badge color='#FFCE80' text={`${recipeInfo.cookTime}m`} icon={<FaClock className='lg:text-xl' />} />
         </div>
-        <h1 className='text-center text-2xl font-bold text-primary-100 md:text-3xl'>{recipeInfo.name}</h1>
-        <p className='text-center text-md text-black md:text-lg'>{recipeInfo.description}</p>
+        <div>
+          <h1 className='text-center text-2xl font-bold text-primary-100 md:text-3xl'>{recipeInfo.name}</h1>
+          <p className='text-center text-md text-black md:text-lg'>{recipeInfo.description}</p>
+        </div>
       </div>
     </section>
   );
